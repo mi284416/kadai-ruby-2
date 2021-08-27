ee4308f9e5c8cf67093e1c3acbb035f9fcee48ed
mi284416 <mi284416@yahoo.co.jp>
2021-08-27 10:23:18 +0000

    saiteishutsu

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,15 +1,16 @@
 num_max = 100
+def fizzbuzz(num)
+  if num % 15 == 0 
+      return "FizzBuzz"      
+  elsif num % 3 == 0
+      return "Fizz"
+  elsif num % 5 == 0
+      return "Buzz"
+  else
+      return num
+  end
+end
+
 (1..num_max).each do |num|
-  def fizzBuzz(num)
-    if num % 15 == 0 
-      puts "FizzBuzz"      
-    elsif num % 3 == 0
-      puts "Fizz"
-    elsif num % 5 == 0
-      puts "Buzz"
-    else
-      puts num
-    end
-  end  
-  puts fizzBuzz(num) 
+  puts fizzbuzz(num)
 end
